4e426d653e73259fafc43a492e716a8d85761c30
tomdziwood <tomdziwood@gmail.com>
2018-11-16 23:31:09 +0100

    Wykonanie konturowania z biblioteką openCV
    Do konturowania służy metoda "cv2.adaptiveThreshold", która posiada dwa warianty - z użyciem średniej lub Gaussa. Program utworzył wykresy dla trzech przypadków dobrania parametrów, później będzie trzeba wybrać któryś z zestawów parametrów, tak aby najlepiej można było wykrywać okręgi.

---
+++ Konturowanie.py
@@ -0,0 +1,93 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+
+
+
+def wykrywanieKonturow(image):
+    print("\tTrwa wykrywanie konturow...")
+
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    """
+    fig, ax = plt.subplots()
+    ax.imshow(gray, cmap='gray')
+    plt.show()
+    """
+
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    """
+    fig, ax = plt.subplots()
+    ax.imshow(blurred, cmap='gray')
+    plt.show()
+    """
+
+    #blockSize = 51
+    #C = 20
+
+    thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)[1]
+    threshMean1 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    threshGauss1 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    threshMean2 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 10)
+    threshGauss2 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 10)
+    threshMean3 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, 20)
+    threshGauss3 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 20)
+
+
+    print("\tKoniec wykrywania konturow.")
+    return [[gray, thresh, threshMean1, threshGauss1, threshMean2, threshGauss2, threshMean3, threshGauss3], ["Oryginalny obraz", "Globalne progowanie (127)",
+                                                                                                              "Adaptacyjne progowanie (średnia), blockSize=11,C=2", "Adaptacyjne progowanie (Gauss), blockSize=11,C=2",
+                                                                                                              "Adaptacyjne progowanie (średnia), blockSize=31,C=10", "Adaptacyjne progowanie (Gauss), blockSize=31,C=10",
+                                                                                                              "Adaptacyjne progowanie (średnia), blockSize=51,C=20", "Adaptacyjne progowanie (Gauss), blockSize=51,C=20"]]
+
+
+
+def main():
+    sciezkaZrodlowa = "zdjecia/1280 x 960/"
+    sciezkaDocelowa = "kontury/1280 x 960/"
+
+    tytul = os.listdir(sciezkaZrodlowa)
+
+    """
+    for i in range(5):
+        image = io.imread(sciezka[i])
+        plot_hist(image)
+    """
+
+
+    #for i in range(len(tytul)):
+    for i in range(len(tytul)):
+        print('Trwa przetwarzanie obrazu o indeksie i =', i, " (" + tytul[i])
+        oryginal = cv2.imread(sciezkaZrodlowa + tytul[i], cv2.IMREAD_COLOR)
+
+
+        print("\tTrwa wykrywanie monet...")
+        images, tytulyWykresow = wykrywanieKonturow(oryginal)
+        """
+        print("type(images) = " + str(type(images)))
+        print("len(images) = " + str(len(images)))
+        print("type(tytulyWykresow) = " + str(type(tytulyWykresow)))
+        print("len(tytulyWykresow) = " + str(len(tytulyWykresow)))
+        """
+
+
+        print("\t\nTrwa rysowanie wykresow...")
+        fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(12, 20.5), subplot_kw={'xticks': [], 'yticks': []})
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.1, wspace=0.1)
+        for j in range(8):
+            ax[int(j / 2), j % 2].set_title(tytulyWykresow[j])
+            ax[int(j / 2), j % 2].imshow(images[j], cmap='gray', aspect='equal', interpolation='bilinear')
+
+
+        print("\tTrwa zapisywanie do pliku jpg...")
+        fig.savefig(sciezkaDocelowa + tytul[i])
+        print("\tPomyslnie zapisano do pliku jpg.\n\n")
+        """
+        print("\tPrzygotowywanie wyswietlenia obrazow w panelu bocznym...")
+        plt.show()
+        print("\tPomyslnie wyswietlono.\n\n")
+        """
+
+
+
+main()
