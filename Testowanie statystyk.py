e740ebc5cfeec429cfd045ddcfaf7d923db35d58
tomdziwood <tomdziwood@gmail.com>
2018-11-16 16:29:34 +0100

    Rozszerzenie programu "Testowanie statystyk.py"
    Teraz program wyświetla statystyki trzech monet "1 zł" z danego obrazka. Jak można zobaczyć na utworzonych wykres, po uruchomieniu programu, jedna złotówka różni się od pozostałych. Chyba ciężko tak tylko z jakichś średnich, median to będzie rozpoznać.

--- Testowanie statystyk.py
+++ Testowanie statystyk.py
@@ -3,7 +3,29 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 
-def plot_hist(img):
+def plot_hist_caly(img):
+    img = img_as_ubyte(img)
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+
+    histo, x = np.histogram(img[:, :, 0], range(0, 256), density=True)
+    ax[0].plot(histo)
+    ax[0].set_xlim(0, 255)
+    ax[0].set_ylim(0, max(histo))
+
+    histo, x = np.histogram(img[:, :, 1], range(0, 256), density=True)
+    ax[1].plot(histo)
+    ax[1].set_xlim(0, 255)
+    ax[1].set_ylim(0, max(histo))
+
+    histo, x = np.histogram(img[:, :, 2], range(0, 256), density=True)
+    ax[2].plot(histo)
+    ax[2].set_xlim(0, 255)
+    ax[2].set_ylim(0, max(histo))
+
+    plt.show()
+
+
+def plot_hist_okrag(img):
     img = img_as_ubyte(img)
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
 
@@ -28,22 +50,15 @@ def plot_hist(img):
     plt.show()
 
 
-def main():
-    print("Poczatek testowania 1zl.")
-
-    sciezkaZrodlowa = "zdjecia/1280 x 960/"
-    tytul = os.listdir(sciezkaZrodlowa)
-
-
+def przypadek01(sciezkaZrodlowa, tytul):
     image = io.imread(sciezkaZrodlowa + tytul[45])
+
     print("\t\nTrwa rysowanie obrazu...")
     fig, ax = plt.subplots(figsize=(8, 6))
     ax.imshow(image, aspect='equal', interpolation='bilinear')
     plt.show()
-
     print("Rysowanie histogramu obrazu...\n")
-    plot_hist(image)
-
+    plot_hist_caly(image)
 
     print("Trwa szykowanie wycinka...")
     wycinek = image[140:415, 715:990]
@@ -56,7 +71,7 @@ def main():
             x[i, j] = i
             y[i, j] = j
     r = xlen / 2
-    wycinek[(x - xlen/2) ** 2 + (y - ylen/2) ** 2 > r ** 2] = [255, 255, 255]
+    wycinek[(x - xlen / 2) ** 2 + (y - ylen / 2) ** 2 > r ** 2] = [255, 255, 255]
 
     print("Trwa rysowanie wycinku...")
     fig, ax = plt.subplots(figsize=(8, 6))
@@ -64,7 +79,86 @@ def main():
     plt.show()
 
     print("Rysowanie histogramu wycinka...")
-    plot_hist(wycinek)
+    plot_hist_okrag(wycinek)
+
+
+def przypadek02(sciezkaZrodlowa, tytul):
+    image = io.imread(sciezkaZrodlowa + tytul[45])
+
+    """
+    print("\t\nTrwa rysowanie obrazu...")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(image, aspect='equal', interpolation='bilinear')
+    plt.show()
+    print("Rysowanie histogramu obrazu...\n")
+    plot_hist_caly(image)
+    """
+
+    print("Trwa szykowanie wycinka...")
+    wycinek = image[280:550, 240:510]
+    xlen = wycinek.shape[0]
+    ylen = wycinek.shape[1]
+    x = np.zeros((xlen, ylen))
+    y = np.zeros((xlen, ylen))
+    for i in range(xlen):
+        for j in range(ylen):
+            x[i, j] = i
+            y[i, j] = j
+    r = xlen / 2
+    wycinek[(x - xlen / 2) ** 2 + (y - ylen / 2) ** 2 > r ** 2] = [255, 255, 255]
+
+    print("Trwa rysowanie wycinku...")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(wycinek, aspect='equal', interpolation='bilinear')
+    plt.show()
+
+    print("Rysowanie histogramu wycinka...")
+    plot_hist_okrag(wycinek)
+
+
+def przypadek03(sciezkaZrodlowa, tytul):
+    image = io.imread(sciezkaZrodlowa + tytul[45])
+
+    """
+    print("\t\nTrwa rysowanie obrazu...")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(image, aspect='equal', interpolation='bilinear')
+    plt.show()
+    print("Rysowanie histogramu obrazu...\n")
+    plot_hist_caly(image)
+    """
+
+    print("Trwa szykowanie wycinka...")
+    wycinek = image[565:835, 467:737]
+    xlen = wycinek.shape[0]
+    ylen = wycinek.shape[1]
+    x = np.zeros((xlen, ylen))
+    y = np.zeros((xlen, ylen))
+    for i in range(xlen):
+        for j in range(ylen):
+            x[i, j] = i
+            y[i, j] = j
+    r = xlen / 2
+    wycinek[(x - xlen / 2) ** 2 + (y - ylen / 2) ** 2 > r ** 2] = [255, 255, 255]
+
+    print("Trwa rysowanie wycinku...")
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.imshow(wycinek, aspect='equal', interpolation='bilinear')
+    plt.show()
+
+    print("Rysowanie histogramu wycinka...")
+    plot_hist_okrag(wycinek)
+
+
+def main():
+    print("Poczatek testowania 1zl.")
+
+    sciezkaZrodlowa = "zdjecia/1280 x 960/"
+    tytul = os.listdir(sciezkaZrodlowa)
+
+    przypadek01(sciezkaZrodlowa, tytul)
+    przypadek02(sciezkaZrodlowa, tytul)
+    przypadek03(sciezkaZrodlowa, tytul)
 
     print("Koniec testowania 1zl.")
 
